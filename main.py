a073e8b56c379f5f6ad6a54e64783d420fc08ba0
liukunr <liukunr@qq.com>
2018-03-08 12:36:08 +0800

    添加投票数据文件格式检查

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ import pandas as pd;
 def read_data(file="./data.txt"):
 	tag=0;
 	data=[];
-	file=open('./data.txt','r');
-	col_num=file.readline();
+	file=open(file,'r');
+	col_num=int(file.readline());
 	for line in file.readlines():
 		linestr=line.strip('\r').strip('\n');
 		if linestr=='':
-			tag=0;
-			continue;
-		if len(data) < tag+1:
-			data.append([list(linestr)]);
+			if tag==len(data):
+				tag=0;
+				continue;
+			else:
+				raise Exception;
 		else:
-			data[tag].append(list(linestr));
+			if col_num==len(linestr):
+				if len(data) < tag+1:
+					data.append([list(linestr)]);
+				else:
+					data[tag].append(list(linestr));
+			else:
+				raise Exception;
 		tag=tag+1;
 	file.close();
 	return data;
@@ -38,6 +45,9 @@ def simple_count(data,is_show=True,show_data=False):
 		print("");
 	return result;
 
-data=read_data();
-for vote in data:
-	simple_count(vote);
+try:
+	data=read_data();
+	for vote in data:
+		simple_count(vote);
+except Exception as e:
+	print("投票数据文件格式错误，请检查！");
